8e7b6fc245eb4a5e4c6cf30f5de6ea7e215f1d49
Matthew Calcote <matthew.calcote@connexta.com>
2018-05-01 07:53:29 -0700

    DDF-0000 Updated windows bin/ddf.bat file

--- distribution/ddf-common/src/main/resources/bin/ddf.bat
+++ distribution/ddf-common/src/main/resources/bin/ddf.bat
@@ -3,11 +3,24 @@ SETLOCAL
 
 SET ARGS=%*
 SET DIRNAME=%~dp0%
+SET SOLR_PID="0"
+SET SOLR_PORT=8994
+SET SOLR_VER=6.6.2
+
+PUSHD ..
+SET DDF_HOME=%CD%
+POPD
 
 :RESTART
 REM Remove the restart file indicator so we can detect later if restart was requested
 IF EXIST "%DIRNAME%\restart.jvm" (
   DEL "%DIRNAME%\restart.jvm"
+)
+
+ECHO "Starting Solr on port %SOLR_PORT%"
+CALL %DDF_HOME%/solr-%SOLR_VER%/bin/solr.cmd start -p %SOLR_PORT%
+IF NOT ERRORLEVEL 0 (
+    ECHO WARNING! Solr start process returned non-zero error code, please check solr logs
 )
 
 REM Actually invoke ddf to gain restart support
@@ -22,6 +35,8 @@ IF "%1"== "daemon" (
     ECHO Restarting JVM...
     GOTO :RESTART
   ) ELSE (
+    echo "Stopping solr process on port %SOLR_PORT%"
+    CALL %DDF_HOME%/solr-%SOLR_VER%/bin/solr.cmd stop -p %SOLR_PORT%
     EXIT /B %RC%
   )
 )
